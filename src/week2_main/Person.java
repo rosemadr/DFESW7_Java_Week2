de0466cc795146dacc0543832e0a52b8090d4f02
rosemadr <madi@redpill.me.uk>
2022-01-11 11:37:30 +0000

    added .equals method to Person class

--- src/week2_main/Person.java
+++ src/week2_main/Person.java
@@ -1,5 +1,7 @@
 package week2_main;
 
+import java.util.Objects;
+
 public class Person {
 
 	// attributes
@@ -36,7 +38,26 @@ public class Person {
 	@Override
 	public String toString() {
 		return "Name: " + getName() + ", age: " + getAge() + ", job title: " + getJobTitle() + ".";
-
+		
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, jobTitle, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && Objects.equals(jobTitle, other.jobTitle) && Objects.equals(name, other.name);
+	}
+	
+	
 
 }
