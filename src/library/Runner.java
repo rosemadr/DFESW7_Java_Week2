9e8e501ea9d74d4af8c87a6df7371ccf37a218ac
rosemadr <madi@redpill.me.uk>
2022-01-16 14:20:59 +0000

    created objects to try out methods

--- src/library/Runner.java
+++ src/library/Runner.java
@@ -3,10 +3,24 @@ package library;
 public class Runner {
 
 	public static void main(String[] args) {
-		
-		User me = new User(0,"Simcock-Brown", "Madeleine", <>, false);
-		
-		me.toString();
+
+		User me = new User(0, "Simcock-Brown", "Madeleine", null, false);
+
+		System.out.println(me.toString());
+
+		Book b1 = new Book("Book", 0, 0, "Mona Lisa Overdrive", "Grafton", 1, "Gibson, William", "SF", false);
+
+		b1.calcBorrowLen(b1);
+
+		System.out.println(b1.toString());
+
+		Library library = new Library();
+
+		library.addToLibrary(b1);
+
+		library.addUser(me);
+
+		library.checkoutItem(b1, me);
 
 	}
 
